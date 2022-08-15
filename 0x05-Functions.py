7bb36d05c93bded20041d0c7e5c7e2f20970f82c
Mariana Contreras Muriel <110686211+marianacontreras21@users.noreply.github.com>
2022-08-14 21:03:12 -0500

    Add files via upload

---
+++ 0x05-Functions.py
@@ -0,0 +1,19 @@
+def suma (sumando1,sumando2):
+    print("Resultado de la suma:", sumando1 + sumando2)
+suma (15, 8)
+suma (44, 13)
+
+def resta (minuendo,sustraendo):
+    print("Resultado de la resta:", minuendo - sustraendo)
+resta (13, 8)
+resta (51, 37)
+
+def multiplicacion (multiplicando,multiplicador):
+    print("Resultado de la multiplicacion:", multiplicando * multiplicador)
+multiplicacion (5, 9)
+multiplicacion (14, 8)
+
+def potencia (base,exponente):
+    print("Resultado de la potencia:", base ** exponente)
+potencia (8, 4)
+potencia (30, 3)
